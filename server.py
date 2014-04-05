d2f912df94321877b7eb51970a7c92bd903c2ff8
Aniruddha Zalani <aniruddha.zalani@gmail.com>
2014-04-06 02:59:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import socket
-
+import socket,sys,commands,thread,os
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -10,12 +9,25 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(10)
+def recieveData(s,conn):
+	print "Recieving data"
+	f=open('recieveFile','w')
+	data = conn.recv(1024)
+	print data
+	json.dump(data,f)
+	print "data recieved"
+	f.close()
+	conn.close()
+#conn, addr = s.accept()
+#thread.start_new_thread(recieveFile,(sl,c))
+while True:
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    conn.send("Connection Established")
+    thread.start_new_thread(recieveData,(s,conn))
+    #data = conn.recv(BUFFER_SIZE)
+    #if not data: break
+    #print "received data:", data
 
-conn, addr = s.accept()
-print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    print "received data:", data
-    conn.send(data)  # echo
 conn.close()
-
+sock.close()
