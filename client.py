5d6ae373f182398449d80bfcea08190a667caa54
Aniruddha Zalani <aniruddha.zalani@gmail.com>
2014-04-06 02:58:26 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python
 
-import socket
-
+import socket,sys,commands,thread,os,json      
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
+def sendData(s):
+	#have to tune it according to our data then will uncomment the lines
+	#log = open ('sendLog','wr')
+	#logs = log.readlines()
+	#line = int(logs[0])
+	#dataFile = open('databaseFile','r')
+	#dataFileLines = dataFile.readlines()
+	#data = dataFileLines[line]
+	#convert required datafiles line into a tuple and save it as a json
+	# send the json file this would make easier to feed into database at the server
+	data = ['[1, "simple", "list"]']
+	jsonfile = open('f','w')
+	json.dump(data,jsonfile)
+	jsonfile.close()
+	jsonfile = open('f','r')
+	data = jsonfile.read()
+	k = len(data)
+	for i in range(0,k,1024):
+		print data
+		s.send(data[i:min(i+1023,k)])
+	#s.send("\nDONE")
+	jsonfile.close()
+	print"File sent successfully\n>"
+	thread.exit()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
+print data
+print "here"
+if data == 'Connection Established':
+	print "hello"
+	sendData(s)
+#s.send(MESSAGE)
 s.close()
 
 print "received data:", data
